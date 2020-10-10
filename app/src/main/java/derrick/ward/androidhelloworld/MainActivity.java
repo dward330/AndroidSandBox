4c8a87ede352b6ea153ed6351c096c679a3d8e64
Derrick <dward330@gmail.com>
2020-10-10 10:38:23 -0700

    Added Button and Edit View and some functionality. Added Local setting for spanish string values.

--- app/src/main/java/derrick/ward/androidhelloworld/MainActivity.java
+++ app/src/main/java/derrick/ward/androidhelloworld/MainActivity.java
@@ -3,6 +3,9 @@ package derrick.ward.androidhelloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    * Event Handler for Saving Message
+    */
+    public void SaveMessage(View view) {
+        EditText enteredMessage = findViewById(R.id.enterMessage);
+        TextView textView = findViewById(R.id.textView);
+
+        // Set textView to show the entered message
+        if (enteredMessage != null && textView != null) {
+            textView.setText(enteredMessage.getText());
+        }
     }
 }
